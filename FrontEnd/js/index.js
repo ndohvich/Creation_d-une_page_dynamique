8cbcf631d88bfb1c9dbb3956f380a434d8c1a5d4
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2022-11-29 19:43:45 +0100

    Initial commit

---
+++ FrontEnd/js/index.js
@@ -0,0 +1,35 @@
+// Déclaration variable et sélection
+
+let nodeMainHTML = document.querySelector(".gallery")
+
+//Fonction fetch allant chercher les infos
+function getData(){ 
+  fetch("http://localhost:5678/api/works/") //<- ici on on vient chercher le data sur le localhost
+  .then((res) => res.json())
+  .then((data) => {
+    for(let i = 0; i< data.length; i ++){ // <- lancement de la boucle
+      // déclarations de toutes les variables nécéssaire pour afficher et attribuer leur valeurs dynamiquement
+        let id = data[i].userId;
+        let title = data[i].title;
+        let imageUrl = data[i].imageUrl;
+        console.log(imageUrl);
+        let categoryId = data[i].categoryId;
+        let category = data[i].category;
+
+      let article= 
+        `
+            <figure>
+                <img crossorigin="anonymous" src='${imageUrl}' alt="${imageUrl}" >
+                <figcaption>${title}</figcaption>
+            </figure>
+        `
+
+        nodeMainHTML.innerHTML += article;
+    }
+  }).catch((err) => {
+    alert(err);
+  })
+
+}
+
+getData();
---
+++ README.md
@@ -0,0 +1,2 @@
+# Creation_page_web_dynamique_avec_JavaScript
+ Mise en place d'un site internet d'une architecte grâce à la création d'un site dynamique avec JavaScript communiquant avec une API
